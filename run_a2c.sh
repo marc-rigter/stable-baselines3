04836d2e9c1b8f7bcfe60e095aa70a0cae411b30
Marc Rigter <marcrigter@gmail.com>
2023-11-26 21:56:45 +0000

    fix script

--- run_a2c.sh
+++ run_a2c.sh
@@ -1,17 +1,17 @@
-python main.py --seed 0 --group "ppo_hopper" --algo "a2c" --env_name "Hopper-v3"  --env_steps 1000000
-python main.py --seed 1 --group "ppo_hopper" --algo "a2c" --env_name "Hopper-v3"  --env_steps 1000000 
-python main.py --seed 2 --group "ppo_hopper" --algo "a2c" --env_name "Hopper-v3"  --env_steps 1000000
-python main.py --seed 3 --group "ppo_hopper" --algo "a2c" --env_name "Hopper-v3"  --env_steps 1000000
-python main.py --seed 4 --group "ppo_hopper" --algo "a2c" --env_name "Hopper-v3"  --env_steps 1000000
+python main.py --seed 0 --group "a2c_hopper" --algo "a2c" --env_name "Hopper-v3"  --env_steps 1000000
+python main.py --seed 1 --group "a2c_hopper" --algo "a2c" --env_name "Hopper-v3"  --env_steps 1000000 
+python main.py --seed 2 --group "a2c_hopper" --algo "a2c" --env_name "Hopper-v3"  --env_steps 1000000
+python main.py --seed 3 --group "a2c_hopper" --algo "a2c" --env_name "Hopper-v3"  --env_steps 1000000
+python main.py --seed 4 --group "a2c_hopper" --algo "a2c" --env_name "Hopper-v3"  --env_steps 1000000
 
-python main.py --seed 0 --group "ppo_cheetah" --algo "a2c" --env_name "HalfCheetah-v3"  --env_steps 1500000
-python main.py --seed 1 --group "ppo_cheetah" --algo "a2c" --env_name "HalfCheetah-v3"  --env_steps 1500000 
-python main.py --seed 2 --group "ppo_cheetah" --algo "a2c" --env_name "HalfCheetah-v3"  --env_steps 1500000
-python main.py --seed 3 --group "ppo_cheetah" --algo "a2c" --env_name "HalfCheetah-v3"  --env_steps 1500000
-python main.py --seed 4 --group "ppo_cheetah" --algo "a2c" --env_name "HalfCheetah-v3"  --env_steps 1500000
+python main.py --seed 0 --group "a2c_cheetah" --algo "a2c" --env_name "HalfCheetah-v3"  --env_steps 1500000
+python main.py --seed 1 --group "a2c_cheetah" --algo "a2c" --env_name "HalfCheetah-v3"  --env_steps 1500000 
+python main.py --seed 2 --group "a2c_cheetah" --algo "a2c" --env_name "HalfCheetah-v3"  --env_steps 1500000
+python main.py --seed 3 --group "a2c_cheetah" --algo "a2c" --env_name "HalfCheetah-v3"  --env_steps 1500000
+python main.py --seed 4 --group "a2c_cheetah" --algo "a2c" --env_name "HalfCheetah-v3"  --env_steps 1500000
 
-python main.py --seed 0 --group "ppo_walker" --algo "a2c" --env_name "Walker2d-v3"  --env_steps 1500000
-python main.py --seed 1 --group "ppo_walker" --algo "a2c" --env_name "Walker2d-v3"  --env_steps 1500000
-python main.py --seed 2 --group "ppo_walker" --algo "a2c" --env_name "Walker2d-v3"  --env_steps 1500000
-python main.py --seed 3 --group "ppo_walker" --algo "a2c" --env_name "Walker2d-v3"  --env_steps 1500000
-python main.py --seed 4 --group "ppo_walker" --algo "a2c" --env_name "Walker2d-v3"  --env_steps 1500000
+python main.py --seed 0 --group "a2c_walker" --algo "a2c" --env_name "Walker2d-v3"  --env_steps 1500000
+python main.py --seed 1 --group "a2c_walker" --algo "a2c" --env_name "Walker2d-v3"  --env_steps 1500000
+python main.py --seed 2 --group "a2c_walker" --algo "a2c" --env_name "Walker2d-v3"  --env_steps 1500000
+python main.py --seed 3 --group "a2c_walker" --algo "a2c" --env_name "Walker2d-v3"  --env_steps 1500000
+python main.py --seed 4 --group "a2c_walker" --algo "a2c" --env_name "Walker2d-v3"  --env_steps 1500000
